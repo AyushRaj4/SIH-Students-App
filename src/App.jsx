1ba3cdf7dc94e626f14b3f5c0c5462466663a8d5
Ayush Raj <163130787+AyushRaj4@users.noreply.github.com>
2024-09-07 19:03:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import "./index.css";
+
+function App() {
+  return <>
+  <Navbar/>
+  <Hero/>
+  </>;
+}
+
+export default App;
---
+++ src/Lists/lists.js
@@ -0,0 +1,18 @@
+export const navMenu = [
+  {
+    id: 1,
+    page: "home",
+  },
+  {
+    id: 2,
+    page: "courses",
+  },
+  {
+    id: 3,
+    page: "instructions",
+  },
+  {
+    id: 4,
+    page: "contact",
+  },
+];
